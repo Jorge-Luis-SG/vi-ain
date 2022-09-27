a2df1221c817f3b43fde4244b9ac5c91064ccd12
Jorge Perez <jorge.jlmp987@gmail.com>
2022-09-27 13:31:43 -0400

    Se añadio el pdf y se borro a un integrante del equipo

--- src/app/pages/blogs/blogs.component.ts
+++ src/app/pages/blogs/blogs.component.ts
@@ -1,18 +1,62 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { SanityImageSource } from '@sanity/image-url/lib/types/types';
+import { Observable } from 'rxjs/internal/Observable';
+import { SanityService } from 'src/app/services/sanity.service';
 
 @Component({
   selector: 'app-blogs',
   templateUrl: './blogs.component.html',
   styleUrls: ['./blogs.component.scss']
 })
+
 export class BlogsComponent implements OnInit {
 
-  constructor(private title:Title) {
+  blogs$: Observable<blog[]>
+  constructor(private title:Title, private sanityService: SanityService) {
     title.setTitle("Vi-ain | Blog | Desarrollo Blockchain");
+
+    const fetchBlogPosts = () => {
+
     }
 
-  ngOnInit(): void {
-  }
+
+    this.blogs$ = this.sanityService.fetch<blog[]>(
+      `*[_type == "post"]{
+        _id,
+        title,
+        _createdAt,
+        author -> {
+            name,
+            image
+        },
+        description,
+        mainImage,
+        slug
+        }`
+     )
+
+
+
+    console.log('hola', this.blogs$[0])
+    }
+
+    ngOnInit(): void {
+    }
 
 }
+
+export interface blog {
+  _id: String,
+  _createdAt: string,
+  title: String,
+  author: {
+    name: string
+    image: string
+  },
+  description: string,
+  mainImage: string,
+  slug: string,
+  poster: SanityImageSource, // = string | SanityReference | SanityAsset | SanityImageObject etc.
+}
+
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
+  production: false,
+  sanity: {
+    projectId: 'xeqvg5n1',
+    dataset: 'production',
+    useCdn: true,
+  },
+  web: {
+    url: 'http://localhost:4200',
+  },
+}
 
 /*
  * For easier debugging in development mode, you can import the following file
